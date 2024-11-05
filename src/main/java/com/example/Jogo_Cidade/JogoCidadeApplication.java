6309e3c37ac1a6cae4ee21c2b21ef7afce974c1e
viniciusbassio <viniciusbassioa11@gmail.com>
2024-11-05 20:09:33 -0300

    Versionamento do jogo

---
+++ src/main/java/com/example/Jogo_Cidade/JogoCidadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jogo_Cidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoCidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jogo_Cidade/JogoCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jogo_Cidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
